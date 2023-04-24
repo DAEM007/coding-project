73ea275d97caa9fc3fc35bb752dd8375e7648d59
Emmanuel <dammylola299@gmail.com>
2023-04-24 11:05:03 +0100

    implemented queues using the linkedList

--- Data-Structures/Queue-LinkedList.js
+++ Data-Structures/Queue-LinkedList.js
@@ -32,17 +32,17 @@ class LinkedListQueue {
     }
 }
 
-const queue = new LinkedListQueue();
-console.log(`is queue empty?: ${queue.isEmpty()}`);
+// const queue = new LinkedListQueue();
+// console.log(`is queue empty?: ${queue.isEmpty()}`);
 
-queue.enqueue('ferrari');
-queue.enqueue('BMW');
-queue.enqueue('lamborghini');
-queue.enqueue('Buggatti');
+// queue.enqueue('ferrari');
+// queue.enqueue('BMW');
+// queue.enqueue('lamborghini');
+// queue.enqueue('Buggatti');
 
-queue.dequeue();
-console.log(`queue size: ${queue.getSize()}`);
-queue.print();
-console.log(queue.peek());
+// queue.dequeue();
+// console.log(`queue size: ${queue.getSize()}`);
+// queue.print();
+// console.log(queue.peek());
 
 
