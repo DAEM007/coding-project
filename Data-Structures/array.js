4e65fdc9c80071df1e15471b9976192facc62c7f
Emmanuel <dammylola299@gmail.com>
2023-04-29 13:55:44 +0100

    Implemented an array

---
+++ Data-Structures/array.js
@@ -0,0 +1,49 @@
+// Implementing an array from scratch
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+    }
+
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index);
+        return item;
+    }
+
+    shiftItems(index) {
+        for(let i = index; i < this.length; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+
+}
+
+const array1 = new MyArray();
+
+array1.push('Hello');
+array1.push('my');
+array1.push('name');
+array1.push('is');
+array1.push('Emmanuel');
+// array1.pop();
+// array1.delete(3);
+console.log(array1);
