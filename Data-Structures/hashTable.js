86d770c520b5e5b31a50f6a42c8a9a7589383402
Emmanuel <dammylola299@gmail.com>
2023-05-02 11:39:51 +0100

    Implemented hashmap without collisions

--- Data-Structures/hashTable.js
+++ Data-Structures/hashTable.js
@@ -16,17 +16,44 @@ class HashMap {
 
     set(key, value) {
         const index = this.hashFunction(key);
-        this.map[index] = value;
+        // this.map[index] = value;
+        if(!this.map[index]) {
+            // create new bucket in that index with key-value pairs
+            this.map[index] = [[key, value]];
+        } else {
+            for(let i = 0; i < this.map[index].length; i++) {
+                // check element in the bucket has key equals passed in key
+                if(this.map[index][i][0] === key) {
+                    this.map[index][i][1] = value;
+                } else {
+                    // if no element...simply push new key-value pairs
+                    this.map[index].push([key, value]);
+                }
+            }
+        }
     }
 
     get(key) {
         const index = this.hashFunction(key);
-        return this.map[index];
+        // return this.map[index];
+        for(let i = 0; i < this.map[index].length; i++) {
+            if(this.map[index][i][0] === key) {
+                // return the value
+                return this.map[index][i][1];
+            }
+        }
+        return undefined;
     }
 
     remove(key) {
         const index = this.hashFunction(key);
-        this.map[index] = undefined;
+        // this.map[index] = undefined;
+        for(let i = 0; i < this.map[index].length; i++) {
+            if(this.map[index][i][0] === key) {
+                this.map[index].splice(i, 1);
+            }
+        }
+        console.log(`${key} not defined!`);
     }
 
     print() {
@@ -42,11 +69,13 @@ class HashMap {
 const hash = new HashMap(50);
 
 hash.set('name', 'Emmanuel');
+// hash.set('name', 'Diana');
+hash.set('mane', 'Damilola');
 hash.set('age', '23');
 hash.set('job', 'software engineer');
 hash.set('company', 'Meta');
 
 console.log(hash.get('name'));
-hash.remove('age');
+hash.remove('black');
 hash.print();
 
