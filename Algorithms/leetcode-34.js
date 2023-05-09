65ad989af4c4b2dd446ff3003cfe73e60a0028f5
Emmanuel <dammylola299@gmail.com>
2023-05-09 07:52:20 +0100

    leetcode-34

--- Algorithms/leetcode-34.js
+++ Algorithms/leetcode-34.js
@@ -14,50 +14,27 @@
 // Output: [-1,-1]
 
 function searchRange(nums, target) {
-    let output = new Array(2);
-    output[0] = findFirst(nums, target);
-    output[1] = findLast(nums, target);
-
-    return output;
-
-}
-
-function findFirst(nums, t) {
-    let left = 0;
-    let right = nums.length - 1;
-    let index = -1;
-
+    left = 0; right= nums.length; 
+    leftIndex = -1; rightIndex = -1;
     while(left <= right) {
-        let middle = left + (right - left) / 2;
-        if(t === nums[middle]) return middle;
-        if(t <= nums[middle]) {
-            right = middle - 1;
-        } else {
-            left = middle + 1;
-        }
+        const middle = Math.floor((left + right) / 2);
+        if(target <= nums[middle]) right = middle - 1;
+        else left = middle + 1;
     }
 
-    return index;
-
-}
-
-function findLast(nums, t) {
-    let left = 0;
-    let right = nums.length - 1;
-    let index = -1;
+    leftIndex = left;
+    left = 0; right = nums.length;
 
     while(left <= right) {
-        let middle = left + (right - left) / 2;
-        if(t === nums[middle]) return middle;
-        if(t >= nums[middle]) {
-            left = middle + 1;
-        } else {
-            right = middle - 1;
-        }
+        const middle = Math.floor((left + right) / 2);
+        if(target < nums[middle]) right = middle - 1;
+        else left = middle + 1; 
     }
 
-    return index;
+    rightIndex = right;
+
+    return leftIndex <= rightIndex ? [leftIndex, rightIndex] : [-1, -1];
 
 }
 
-console.log(searchRange([1,2,3,4,5,6,7,8,8,9,10], 8));
+console.log(searchRange([5,7,7,8,8,10], 8));
