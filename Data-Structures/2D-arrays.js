6395d643cd1bdc9ad9eb3f83d373bc76a564e941
Emmanuel <dammylola299@gmail.com>
2023-05-10 15:49:57 +0100

    studied 2-D arrays

---
+++ Data-Structures/2D-arrays.js
@@ -0,0 +1,11 @@
+const mathScore = [
+    ['John Doe', 10, 35, 'D'],
+    ['Jane Doe', 10, 52, 'B'],
+    ['Peter lin', 5, 50, 'C'],
+    ['Jet li', 10, 30, 'D'],
+    ['Ben Light', 20, 70, 'A'],
+]
+
+console.log(mathScore[0][0]);
+console.log(mathScore[mathScore.length - 1]);
+console.log(mathScore[mathScore.length - 1][mathScore[mathScore.length - 1].length - 1]);
