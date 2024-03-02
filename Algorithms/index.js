36aacd7b6201a173b238942bf41d0d46c0bb61bb
DAMILOLA <dammylola299@gmail.com>
2024-03-02 11:58:36 +0100

    updates to algorithm

--- Algorithms/index.js
+++ Algorithms/index.js
@@ -131,4 +131,25 @@ function printAllPairs(n) {
     }
 }
 
-console.log(printAllPairs(6));
+// run-time of o(n^2)
+// console.log(printAllPairs(6));
+
+// challenge: Print at least 5
+function logAtLeast(n) {
+    for (let i = 0; i < Math.max(5, n); i++) {
+        console.log(i);
+    }
+}
+
+// run-time of o(n)
+// console.log(logAtLeast(10)); 
+
+// challenge: Print at most 5
+function logAtMost(n) {
+    for (let i = 0; i < Math.min(5, n); i++) {
+        console.log(i);
+    }
+}
+
+// run-time of o(1)
+// console.log(logAtMost(10000));
