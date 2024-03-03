bf94cc328c2efbc7ccf88069c402c71fc92dea5b
DAMILOLA <dammylola299@gmail.com>
2024-03-03 18:57:35 +0100

    updates to dsa-course

---
+++ DSA-COURSE-FROM-SCRATCH/Analysing-arrays-and-objects.js
@@ -0,0 +1,20 @@
+// ============== ANALYSING THE PERFORMANCE OF ARRAYS AND OBJECTS ================
+
+// Analysis for the big - o of an object includes the following:
+
+// Object.keys() - o(N)
+// Object.value() - o(N)
+// Object.hasOwn() - o(1)
+// Object.entries() - o(N)
+
+// Analysis for the big - o of an Array includes the following:
+
+// Array.push() - o(1)
+// Array.pop() - o(1)
+// Array.shift() - o(N)
+// Array.unShift() - o(N)
+// Array.concat() - o(N)
+// Array.sort() - o(NlogN)
+// Array.slice() - o(N)
+// Array.splice() - o(N)
+
