d8423f964c795e409642cc678ea9905c32dee485
DAMILOLA <dammylola299@gmail.com>
2024-03-04 23:00:59 +0100

    updates to dsa-course

--- DSA-COURSE-FROM-SCRATCH/intro-to-problem-solving.js
+++ DSA-COURSE-FROM-SCRATCH/intro-to-problem-solving.js
@@ -1,4 +1,4 @@
-// =========== How do you improve your problem solving skills =============
+// =========== How do you improve your problem solving skills? =============
 
 // 1. Devise a plan for solving problem.
 // 2. Master common problem solving patterns.
@@ -11,3 +11,4 @@
 // 5. look back and refactor.
 
 
+// Note: communication is key in your interviews.
