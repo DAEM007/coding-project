ab19d7d2a13fe504a9e6b4a8525905a74548446f
DAMILOLA <dammylola299@gmail.com>
2024-03-17 20:28:20 +0100

    updates to dsa-course-from-scratch

--- DSA-COURSE-FROM-SCRATCH/4-problem-solving-patterns/1-frequency-counter.js
+++ DSA-COURSE-FROM-SCRATCH/4-problem-solving-patterns/1-frequency-counter.js
@@ -47,10 +47,12 @@ function same3(arr1, arr2) {
     let frequencyCounter2 = {};
 
     for(let val of arr1) {
-        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1; // You can further break this down, if you want.
+        frequencyCounter1[val] ? frequencyCounter1[val]++ : frequencyCounter1[val] = 1;
+        // frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1; // This is the refactored version of the above.
     }
     for(let val of arr2) {
-        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1; // You can further break this down, if you want.
+        frequencyCounter2[val] ? frequencyCounter2[val]++ : frequencyCounter2[val] = 1;
+        // frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1; // This is the refactored version of the above.
     }
     // console.log(frequencyCounter1, frequencyCounter2); // This is to track and check what we have in the counter...completely optional. You can skip this part of the code.
     for(let key in frequencyCounter1) {
@@ -78,10 +80,10 @@ function validAnagram(str1, str2) {
     let frequencyCounter2 = {};
 
     for(let char of str1) {
-        frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1;
+        frequencyCounter1[char] ? frequencyCounter1[char]++ : frequencyCounter1[char] = 1;
     }
     for(let char of str2) {
-        frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1;
+        frequencyCounter2[char] ? frequencyCounter2[char]++ : frequencyCounter2[char] = 1;
     }
     console.log(frequencyCounter1, frequencyCounter2);
     for(let key in frequencyCounter1) {
