6f8a81a42f2e1694801b149b87eaf39fc8ed6355
DAMILOLA <dammylola299@gmail.com>
2024-03-17 19:00:45 +0100

    update to dsa-from-scratch

--- DSA-COURSE-FROM-SCRATCH/4-problem-solving-patterns/1-frequency-counter.js
+++ DSA-COURSE-FROM-SCRATCH/4-problem-solving-patterns/1-frequency-counter.js
@@ -70,3 +70,32 @@ function same3(arr1, arr2) {
 // console.log(same3([1,2,3], [9,4,1]));
 
 //  challenge (Anagram): Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase or name formed by the rearranging the letters of another, such as "cinema", formed from "iceman". 
+function validAnagram(str1, str2) {
+    if(str1.length !== str2.length) {
+        return false;
+    }
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+
+    for(let char of str1) {
+        frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1;
+    }
+    for(let char of str2) {
+        frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1;
+    }
+    console.log(frequencyCounter1, frequencyCounter2);
+    for(let key in frequencyCounter1) {
+        if(frequencyCounter2[key] !== frequencyCounter1[key]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+// console.log(validAnagram('', ''));
+// console.log(validAnagram('aaz', 'zza'));
+// console.log(validAnagram('rat', 'car'));
+// console.log(validAnagram('anagram', 'nagaram'));
+// console.log(validAnagram('awesome', 'awesom'));
+// console.log(validAnagram('qwerty', 'qeywrt'));
+// console.log(validAnagram('texttwisttime', 'timetwisttext'));
