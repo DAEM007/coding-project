9c4060aadaf5352184f92c30ffaeed1bc8ebc1c7
DAMILOLA <dammylola299@gmail.com>
2024-03-20 22:27:40 +0100

    updates to sliding-window

--- DSA-COURSE-FROM-SCRATCH/4-problem-solving-patterns/3-sliding-window.js
+++ DSA-COURSE-FROM-SCRATCH/4-problem-solving-patterns/3-sliding-window.js
@@ -2,3 +2,14 @@
 
 // ==== Sliding window pattern =====
 
+// challenge: write a function called maxSubarraySum which accepts an array of integers and a number called n. The function should calculate the maximum sum of n consecutive elements in the array.
+
+function maxSubarraySum () {
+    
+};
+
+console.log(maxSubarraySum([1,2,5,2,8,1,5], 2));
+console.log(maxSubarraySum([1,2,5,2,8,1,5], 4));
+console.log(maxSubarraySum([4,2,1,6], 1));
+console.log(maxSubarraySum([4,2,1,6,2], 4));
+console.log(maxSubarraySum([], 4));
